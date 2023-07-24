0f36f522d27d1032950f197b35f393035985f4e3
ivan <ivan-guita@hotmail.com>
2023-07-24 10:45:54 -0300

    cessão de validação de formulario  finalizada

--- index.js
+++ index.js
@@ -3,25 +3,47 @@ const express = require("express");
 const flash = require("express-flash");
 const app = express();
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+//esse valor aleatorio vai ser usado como base pra gerar os cookies de validação 
+app.use(cookieParser("jcmdkljkldsjfldck"));
+
 
 
 app.use(session({
     secret:'Keyboard cat',
     resave: false,
     saveUninitialized: true,
-    cookie:{secure: true }
+    cookie:{maxAge: 60000} //60000 define em quanto tempo o cokies expira eu coloquei uma hora pq isso e um form de teste 
 }))
+
+
+
+
 
 
 app.use(flash())
 
 app.get('/',(req,res)=>{
 
-  res.render("index");
+   var emailError = req.flash('emailError');
+   var pontosError = req.flash('pontosError'); 
+   var nomeError = req.flash('nomeError');
+   var email = req.flash('email');
+
+
+   emailError  = (emailError   == undefined   || emailError.length  == 0)? undefined: emailError;
+   email = (email == undefined || email.length == 0) ? "" : email;
+   pontosError = (pontosError  == undefined   || pontosError.length == 0)? undefined: pontosError;
+   nomeError   = (nomeError    == undefined   || nomeError.length   == 0)? undefined: nomeError;
+
+ 
+
+   res.render("index",{emailError,pontosError,nomeError,email: email});
 
 })
 
@@ -53,7 +75,15 @@ app.post('/form',(req,res)=>{
      }
 
     if(emailError != undefined || pontosError != undefined || nomeError != undefined){
+        //Para que as mensagens de erro possam ser lidas pelo cliente 
+        //quando o erro aocntecer elas precisam ser enviadas para o formulario 
+        //junto com a rota de redirecionamento
+        //pra isso eu uso a função flash da classe de requisição 
         
+        req.flash('emailError', emailError);
+        req.flash('pontosError', pontosError);
+        req.flash('nomeError', nomeError);
+
         res.redirect("/");
 
     }else{
